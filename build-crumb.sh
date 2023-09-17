6cfae66d0d63326261965fded1c06ba86550f03c
Ron Ilan <ronilan.webdev@gmail.com>
2023-09-16 23:34:13 -0700

    Simplified event triggering. Fixed issue with multiple listeners modifying state due to same event. better build-crumb.sh Tweaks.

--- build-crumb.sh
+++ build-crumb.sh
@@ -1,11 +1,38 @@
 #!/bin/sh
+default="v0.0.1"
 
-tag="v0.0.1"
+arg=${1:-$default}
+len=${#arg}
+tag=$arg || $default
+
 into=".tmp~crumb~inflate"
 
-git clone --branch $tag --depth 1 https://github.com/liam-ilan/crumb.git $into
-cd $into || exit
-gcc src/*.c -Wall -lm -o crumb 
-cd .. 
-mv ./$into/crumb crumb
-rm -rf $into
+build_and_clean() {
+  gcc src/*.c -Wall -lm -o crumb
+  cd ..
+  mv ./$into/crumb crumb
+  rm -rf $into
+}
+
+if [ "$len" = 40 ] # its is a sha
+then
+  printf "\e[32m Building from SHA %s \e[0m \n" "$arg"
+  git clone https://github.com/liam-ilan/crumb.git $into
+  cd $into || exit
+  git reset --hard "$arg"
+
+  build_and_clean
+
+  printf "\e[32m Crumb built successfully from %s! \e[0m \n" "$arg"
+elif [ "$(printf '%s' "$tag" | cut -c 1)" = "v" ] # its is a sha
+then # its is a tag
+  printf "\e[32m Building version %s \e[0m \n" "$tag"
+  git clone -c advice.detachedHead=false --branch "$tag" --depth 1 https://github.com/liam-ilan/crumb.git $into
+  cd $into || exit
+
+  build_and_clean
+
+  printf "\e[32m Crumb version %s built successfully!\e[0m \n" "$tag"
+else
+  printf "\e[31m %s not valid version tag nor SHA \e[0m \n" "$arg"
+fi
